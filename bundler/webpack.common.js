c43f440c33670ebaeed4aa7d363403cda4bf4b45
Rohan Mittal <mittal.rohan2010@gmail.com>
2025-12-26 21:30:01 +1100

    Fix GitHub Pages public path

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -8,7 +8,8 @@ module.exports = {
     output:
     {
         filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/flow-state/'
     },
     devtool: 'source-map',
     plugins:
